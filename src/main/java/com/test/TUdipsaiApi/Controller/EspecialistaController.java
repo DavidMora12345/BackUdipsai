735b07b4bf0c1f7985fd8879405b571d8ddc7d21
User <User@DESKTOP-UKIELNS>
2024-06-05 19:43:35 -0500

    Commit Especialistas actualizado v5

--- src/main/java/com/test/TUdipsaiApi/Controller/EspecialistaController.java
+++ src/main/java/com/test/TUdipsaiApi/Controller/EspecialistaController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/especialistas")
+@CrossOrigin(origins = "*")
 public class EspecialistaController {
 
     @Autowired
--- src/main/java/com/test/TUdipsaiApi/Controller/InstitucionEducativaController.java
+++ src/main/java/com/test/TUdipsaiApi/Controller/InstitucionEducativaController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/instituciones")
+@CrossOrigin(origins = "*")
 public class InstitucionEducativaController {
 
     private final InstitucionEducativaService institucionEducativaService;
